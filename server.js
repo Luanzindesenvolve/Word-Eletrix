67ed0bde171f88324a66a51782ffe7a520d60002
Luanzindesenvolve <164371570+Luanzindesenvolve@users.noreply.github.com>
2024-07-28 00:43:03 -0300

    Update server.js

--- server.js
+++ server.js
@@ -1,8 +1,5 @@
 const express = require('express');
 const path = require('path');
-const fs = require('fs'); // Certifique-se de que fs está importado corretamente
-const api = require('./api');  // Importa o módulo da API
-const app = express();
 
 // Define o diretório público para servir os arquivos estáticos
 app.use(express.static(path.join(__dirname)));
@@ -10,21 +7,6 @@ app.use(express.static(path.join(__dirname)));
 // Rota para servir o arquivo index.html
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'index.html'));
-});
-// Rota para servir arquivos JSON da API
-app.get('/api/:category', (req, res) => {
-  const category = req.params.category;
-  const filePath = path.join(__dirname, 'db', `${category}.json`);
-
-  fs.readFile(filePath, (err, data) => {
-    if (err) {
-      return res.status(404).json({
-        status: false,
-        mensagem: `Arquivo ${category}.json não encontrado.`,
-      });
-    }
-    res.json(JSON.parse(data));
-  });
 });
 // Rota dinâmica para servir arquivos HTML e TXT sem a extensão
 app.get('/:page', (req, res) => {
