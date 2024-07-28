faffcce02c0d07b324c8cc5b25c690d43b5a187d
Luanzindesenvolve <164371570+Luanzindesenvolve@users.noreply.github.com>
2024-07-28 16:32:17 -0300

    Add files via upload

---
+++ dados/contasOnly.js
@@ -0,0 +1,109 @@
+const fs = require('fs')
+
+var contaOnly = [`
+Luiza russo (mansão maromba)
+via telegram> https://t.me/+8cJ7yGnq5fI2YjYx
+`,`
+Luiza blue 
+via telegram> https://t.me/+JaX8wY_45843ZTBh
+`,`
+Isadora vale 
+via telegram> https://t.me/+smb4i0bxRl41ZjRh
+`,`
+Ingrid bianchi
+via telegram> https://t.me/+RJtf2TqTQ8NjZjlh
+`,`
+Lizy Donato 
+via telegram> https://t.me/+gmSjm2WZnb41OGEx
+`,` 
+Feh Galvão
+via telegram> https://t.me/+naeQ1pp8ajswMjgx
+`,`
+Tatyzack
+via telegram> https://t.me/+NvglGFhOLSY5NmZ
+`,`
+Ayrla Souza 
+via telegram> https://t.me/+XLziPd47MWo2N2Y
+`,`
+ALINE FARIA
+via telegram> https://t.me/+j1xp_hmXrx5lZWMx
+`,`
+KINE CHAN 
+via telegram> https://t.me/+4MnDFWV0oDtlOWVh
+`,`
+DJ NATHI
+via telegram> https://t.me/+6Z5vuVApT-1lYjEx
+`,`
+BELE KAFFER
+via telegram> https://t.me/+70DMw3iTzb0yMzEx
+`,`
+only dos lestes
+via telegram> https://t.me/cadelinhasredansbr
+`,`
+Lua Santa
+via telegram> https://t.me/+tX4sSnwJYBI3NGMx
+`,`
+Lua Uchiha
+via telegram> https://t.me/+nW1We0oSDuUxZWFh
+`,`
+Cibelly Ferreira (prof do tik tok)
+via telegram> https://t.me/+SkelQs1wPJ1iYmNh
+`,`
+Catarina Paulino
+via telegram> https://t.me/+JwZmPiNqSDI5NWUx
+`,`
+Jennie s
+via telegram> https://t.me/+lPvd-vfwxSxjMjkx
+`,`
+Isabella Ramirez
+via telegram> https://t.me/+9Ow2dG6RMjMwOGNh
+`,`
+Irmãs Burch
+via telegram> https://t.me/+qLHY0yEPajI1MzEx
+`,`
+Mia Khalifa
+via telegram> https://t.me/+Ky3nc1oowKY4Mjkx
+`,`
+Martina Oliver
+via telegram> https://t.me/+roO0b344XJc0ZGQx
+`,`
+Misato
+via telegram> https://t.me/+-zg9uNZfuFtiMGYx
+`,`
+Michelle rabbit
+via telegram> https://t.me/+scMFC3r5t70zZWNh
+`,`
+Ma1adinha
+via telegram> https://t.me/+jIYg2RAtpBQyNzYx
+`,`
+Pamella Makeup
+via telegram> https://t.me/+EvJ3KIiKKm0yOTA5
+`,`
+Paola Gonzalez
+via telegram> https://t.me/+4qhrVK0G-IM3NTRh
+`,`
+Sofia simens
+via telegram> https://t.me/+KrhtdA5LtOxlYWYx
+`,`
+Tia Brit
+via telegram> https://t.me/+nPC3Pw6jDhsxM2Yx
+`,`
+Taiane Aburquerque
+via telegram> https://t.me/+fhRSxuBzEbc5MTVh
+`,`
+Sarah Carvalho
+via telegram> https://t.me/+WQWQfzhaadYzZDAx
+`,`
+Paola Hard
+via telegram> https://t.me/+eYiTWQbYYFU2YjNh
+`,`
+Queen Egril
+via telegram> https://t.me/+ccV_bTEIFus0MmQx
+`,`
+Quinn Wild
+via telegram> https://t.me/+xygzLnkF72A0YjAx
+`
+]
+module.exports = {
+contaOnly
+}
