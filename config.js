d6f5c0f1abcf600b2cbc0797f8641bb1d33fe884
Luanzindesenvolve <164371570+Luanzindesenvolve@users.noreply.github.com>
2024-08-18 15:08:25 -0300

    Update config.js

--- config.js
+++ config.js
@@ -52,26 +52,25 @@ function formatDuration(duration) {
 
 function LetradaMusica(musica){
 	return new Promise(async(resolve, reject) => {
-   		axios.get('https://www.musixmatch.com/search/' + musica)
+   		axios.get('https://www.musixmatch.com/search?query=' + encodeURIComponent(musica))
    		.then(async({ data }) => {
-   		const $ = cheerio.load(data)
-   		const resultado = {};
-   		let limk = 'https://www.musixmatch.com'
-   		const link = limk + $('div.media-card-body > div > h2').find('a').attr('href')
+   			const $ = cheerio.load(data);
+   			const resultado = {};
+   			const limk = 'https://www.musixmatch.com';
+   			const link = limk + $('div.media-card-body > div > h2').find('a').attr('href');
 	   		await axios.get(link)
 	   		.then(({ data }) => {
-		   		const $$ = cheerio.load(data)
-		   		resultado.ImagemMusic = 'https:' + $$('div.col-sm-1.col-md-2.col-ml-3.col-lg-3.static-position > div > div > div').find('img').attr('src')
+		   		const $$ = cheerio.load(data);
+		   		resultado.ImagemMusic = 'https:' + $$('div.col-sm-1.col-md-2.col-ml-3.col-lg-3.static-position > div > div > div').find('img').attr('src');
 		  		$$('div.col-sm-10.col-md-8.col-ml-6.col-lg-6 > div.mxm-lyrics').each(function(a,b) {
-		   resultado.LetraDaMusica = $$(b).find('span > p > span').text() +'\n' + $$(b).find('span > div > p > span').text()
-		   })
-	   })
-	   resolve(resultado)
-   })
-   .catch(reject)
-   })
+		  			resultado.LetraDaMusica = $$(b).find('span > p > span').text() +'\n' + $$(b).find('span > div > p > span').text();
+		  		});
+	  	})
+	  	resolve(resultado);
+  	})
+  	.catch(reject);
+	});
 }
-
 
 async function XvideosDL(url) {
   try {
