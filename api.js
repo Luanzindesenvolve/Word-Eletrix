1213b6789804dba1eb99ba029ccea85da013b1cf
Luanzindesenvolve <164371570+Luanzindesenvolve@users.noreply.github.com>
2024-12-01 15:15:35 -0300

    Update api.js

--- api.js
+++ api.js
@@ -4297,13 +4297,13 @@ router.get('/attp5', (req, res) => {
 
 
 //Photooxy 
-const KEY_FUT = 'live_571515d2dc252f3cd644521ba9be2d'; // Substitua pela sua chave
+const KEY_FUT = 'live_9f96c55d1086375d07ad9c3fcb9a19'; // Substitua pela sua chave
 // Rota para obter a artilharia do Brasileirão
 router.get('/artilheiro', async (req, res) => {
   try {
     const response = await axios.get('https://api.api-futebol.com.br/v1/campeonatos/10/artilharia', {
       headers: {
-        'Authorization': 'Bearer live_56c73242d991a238681e22a8866d11' 
+        'Authorization': 'Bearer live_9f96c55d1086375d07ad9c3fcb9a19' 
       }
     });
 
