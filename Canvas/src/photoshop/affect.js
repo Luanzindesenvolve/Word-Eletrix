ac7459e1b3d1a744642824ec6307335cffac016f
Luanzindesenvolve <164371570+Luanzindesenvolve@users.noreply.github.com>
2025-03-22 16:50:42 -0300

    Update affect.js

--- Canvas/src/photoshop/affect.js
+++ Canvas/src/photoshop/affect.js
@@ -1,10 +1,14 @@
+const Jimp = require("jimp");
+
 module.exports = async (image) => {
-        if (!image) throw new Error("Parâmetro <image> não definido!");
-        const jimp = require("jimp");
-        let base = await jimp.read("https://i.imgur.com/0dyCAZL.png");
-        let img = await jimp.read(image);
-        img.resize(200, 157);
-        base.composite(img, 180, 383);
-        let raw = await base.getBufferAsync("image/png");
-        return raw;
-}
+    if (!image) throw new Error("Parâmetro <image> não definido!");
+    
+    let base = await Jimp.read("https://i.imgur.com/0dyCAZL.png");
+    let img = await Jimp.read(image);
+    
+    img.resize(200, 157);
+    base.composite(img, 180, 383);
+    
+    let raw = await base.getBufferAsync(Jimp.MIME_PNG);
+    return raw;
+};
