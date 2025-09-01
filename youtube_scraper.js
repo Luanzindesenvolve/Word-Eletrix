5b7b2793cf66f70a5c28002c611900081f071f24
Luanzindesenvolve <164371570+Luanzindesenvolve@users.noreply.github.com>
2025-09-01 11:19:46 -0300

    Update youtube_scraper.js

--- youtube_scraper.js
+++ youtube_scraper.js
@@ -1,5 +1,6 @@
 // youtube_scraper.js
+// Permite usar @vreden/youtube_scraper em CommonJS
 module.exports = (async () => {
   const mod = await import("@vreden/youtube_scraper");
-  return mod;
+  return mod.default || mod; // garante compatibilidade
 })();
