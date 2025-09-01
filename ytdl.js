a98b45b0a223ba66cba14bec8f16b88eea920ff8
Luanzindesenvolve <164371570+Luanzindesenvolve@users.noreply.github.com>
2025-09-01 11:34:55 -0300

    Update ytdl.js

--- ytdl.js
+++ ytdl.js
@@ -2,7 +2,7 @@
 // Wrapper para usar @hiudyy/ytdl em projeto CommonJS
 
 module.exports = (async () => {
-  const mod = await import("@hiudyy/ytdl");
+  const mod = await import("@distube/ytdl-core");
   // alguns pacotes exportam em .default, outros direto
   return mod.default || mod;
 })();
