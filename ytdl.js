cdfd599b5cbb4d6b1dbaa03f0c8e7cea74b2c637
Luanzindesenvolve <164371570+Luanzindesenvolve@users.noreply.github.com>
2025-09-01 11:45:16 -0300

    Delete ytdl.js

--- ytdl.js
+++
@@ -1,8 +0,0 @@
-// ytdl.js
-// Wrapper para usar @hiudyy/ytdl em projeto CommonJS
-
-module.exports = (async () => {
-  const mod = await import("@distube/ytdl-core");
-  // alguns pacotes exportam em .default, outros direto
-  return mod.default || mod;
-})();
